7a480b1d9eb4612903652eaf7c6aa48bc79ff136
sugiyama-k <sugiyama-k@illmatics.co.jp>
2019-10-18 13:54:25 +0900

    バリデーション箇所を修正

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,7 +8,7 @@
             <form action=" {{ url('update', $user->id) }} " method="post">
                 @csrf
 
-                @if($errors->has('name'))<span>{{ $errors->first('name') }}</span>@endif
+                @include('layouts.alert', ['errors' => $errors])
 
                 <div class="form-group">
                     <label for="name">氏名</label>
@@ -17,7 +17,7 @@
 
                 <ul class="list-inline">
                     <li class="list-inline-item">
-                        <a href="http://localhost/users/1" class="btn btn-secondary">キャンセル</a>
+                        <a href="{{ url('list') }}" class="btn btn-secondary" >キャンセル</a>
                     </li>
                     <li class="list-inline-item">
                         <button type="submit" class="btn btn-primary">更新</button>
