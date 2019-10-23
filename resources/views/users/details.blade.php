235ec98eb76876b666aee690fc7b465f7fb70570
sugiyama-k <sugiyama-k@illmatics.co.jp>
2019-10-23 15:23:16 +0900

    インデント致しました。

--- resources/views/users/details.blade.php
+++ resources/views/users/details.blade.php
@@ -5,30 +5,30 @@
 
 
     <main class="container py-3">
-    <div class="row">
-        <div class="col-sm">
-            <ul class="list-inline">
-                <li class="list-inline-item">
-                    <a href="http://localhost/users" class="btn btn-light">戻る</a>
-                </li>
-                <li class="list-inline-item">
-                    <a href="{{ url('users/'.$user->id.'/edit')  }}" class="btn btn-success">編集</a>
-                </li>
-                <li class="list-inline-item">
-                    <form action="{{ url('users/'.$user->id) }}" method="POST">
-                        @csrf
-                        {{ method_field('DELETE') }}
-                        <button type="submit" class="btn btn-danger">削除</button>
-                    </form>
-                </li>
-            </ul>
+        <div class="row">
+            <div class="col-sm">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a href="http://localhost/users" class="btn btn-light">戻る</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="{{ url('users/'.$user->id.'/edit')  }}" class="btn btn-success">編集</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <form action="{{ url('users/'.$user->id) }}" method="POST">
+                            @csrf
+                            {{ method_field('DELETE') }}
+                            <button type="submit" class="btn btn-danger">削除</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-sm">
-            <table class="table">
-                <tbody>
+        <div class="row">
+            <div class="col-sm">
+                <table class="table">
+                    <tbody>
                     <tr>
                         <th>ID</th>
                         <td>{{ $user->id }}</td>
@@ -37,10 +37,11 @@
                         <th>氏名</th>
                         <td>{{ $user->name }}</td>
                     </tr>
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
 
 @endsection
+
