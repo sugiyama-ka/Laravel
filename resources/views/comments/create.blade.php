85c7d6ea56165250ac2ccb0fade349525a65bf63
sugiyama-k <sugiyama-k@illmatics.co.jp>
2019-10-23 15:16:08 +0900

    コメント作成ページを作成致しました。

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,41 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<main class="container py-3">
+    <main class="container py-3">
 
         <div class="row">
-        <div class="col-sm">
-            <form action="{{ route('comment_store', $book_id) }}" method="post">
-                @csrf
+            <div class="col-sm">
+                <form action="{{ route('comment_store', $book_id) }}" method="post">
+                    @csrf
 
-                @include('layouts.alert', ['errors' => $errors])
-                <div class="form-group">
-                    <label for="user_id">ユーザー</label>
-                    <select class="form-control" id="user_id" name="user">
-                    @foreach($users as $user)
-                            <option value="{{ $user->id }}">{{ $user->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
+                    @include('layouts.alert', ['errors' => $errors])
+                    <div class="form-group">
+                        <label for="user_id">ユーザー</label>
+                        <select class="form-control" id="user_id" name="user">
+                            @foreach($users as $user)
+                                <option value="{{ $user->id }}">{{ $user->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
 
+                    <div class="form-group">
+                        <label for="message">メッセージ</label>
+                        <input type="text" class="form-control " id="message" name="message" value="">
+                    </div>
 
-                <div class="form-group">
-                    <label for="message">メッセージ</label>
-                    <input type="text" class="form-control " id="message" name="message" value="">
-                </div>
-
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a href="http://localhost/" class="btn btn-secondary">キャンセル</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <button type="submit" class="btn btn-primary">投稿</button>
-                    </li>
-                </ul>
-            </form>
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            <a href="http://localhost/" class="btn btn-secondary">キャンセル</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <button type="submit" class="btn btn-primary">投稿</button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
 
 @endsection
 
