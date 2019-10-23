71ec4569190bf06f4814367caafc699b5e5e8e0e
sugiyama-k <sugiyama-k@illmatics.co.jp>
2019-10-23 15:21:29 +0900

    ユーザー作成ページにicon画像を追加出来るように致しました。

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,26 +4,33 @@
     <main class="container py-3">
         @include('layouts.alert', ['errors' => $errors])
         <div class="row">
-        <div class="col-sm">
-            <form action="{{ url('users')  }}" method="post">
-                @csrf
+            <div class="col-sm">
+                <form action="{{ url('users')  }}" method="post" enctype="multipart/form-data">
+                    @csrf
 
-                <div class="form-group">
-                    <label for="name">氏名</label>
-                    <input type="text" class="form-control " id="name" name="name" value="{{ old('name') }}">
-                </div>
+                    <div class="form-group">
+                        <label for="name">氏名</label>
+                        <input type="text" class="form-control " id="name" name="name" value="{{ old('name') }}">
+                    </div>
 
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a href="{{ url('users') }}" class="btn btn-secondary">キャンセル</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <button type="submit" class="btn btn-primary">作成</button>
-                    </li>
-                </ul>
-            </form>
+                    <div class="form-group">
+                        <label for="name">アイコン画像</label><br>
+                        <input type="file" name="icon" value=""/>
+                    </div>
+
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            <a href="{{ url('users') }}" class="btn btn-secondary">キャンセル</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <button type="submit" class="btn btn-primary">作成</button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
 
 @endsection
+
+
